0be3289f5f93a770d6e3d2c9919f8d0f2402b704
Dan Barclay <barclaysd@me.com>
2024-12-05 00:38:35 +0000

    feat: day 4 part 2

--- day-04/src/main.rs
+++ day-04/src/main.rs
@@ -83,14 +83,57 @@ fn get_word_search_count(file_path: &str, search_word: &str) -> i32 {
     positions.len() as i32
 }
 
+fn get_word_search_from_x_formation_count(file_path: &str, search_word: &str) -> i32 {
+    let file_contents = fs::read_to_string(file_path)
+        .expect("Should have been able to read the file");
+
+    let grid: Vec<Vec<char>> = file_contents
+        .lines()
+        .map(|line| line.chars().collect())
+        .collect();
+
+    let mut x_formation_count = 0;
+    for x in 0..grid.len() {
+        for y in 0..grid[x].len() {
+            if get_is_x_formation(&grid, x as i32, y as i32, search_word) {
+                x_formation_count += 1
+            }
+        }
+    }
+
+    x_formation_count
+}
+
+fn get_is_x_formation(chars: &Vec<Vec<char>>, x: i32, y: i32, search_word: &str) -> bool {
+    let c = get_char_at(chars, x, y);
+    let tl = get_char_at(chars, x - 1, y - 1);
+    let tr = get_char_at(chars, x + 1, y - 1);
+    let bl = get_char_at(chars, x - 1, y + 1);
+    let br = get_char_at(chars, x + 1, y + 1);
+
+    [format!("{tl}{c}{br}"), format!("{bl}{c}{tr}")]
+        .iter()
+        .all(|word| word == search_word || *word == search_word.chars().rev().collect::<String>())
+}
+
+fn get_char_at(chars: &Vec<Vec<char>>, x: i32, y: i32) -> char {
+    *chars
+        .get(x as usize)
+        .and_then(|c| c.get(y as usize))
+        .unwrap_or(&'.')
+}
+
+
 fn main() {
     let count = get_word_search_count("./input.txt", "XMAS");
     println!("Part 1: {}", count);
+    let count_from_x_formation = get_word_search_from_x_formation_count("./input.txt", "XMAS");
+    println!("Part 2: {}", count_from_x_formation);
 }
 
 #[cfg(test)]
 mod tests {
-    use crate:: get_word_search_count;
+    use crate::{get_word_search_count, get_word_search_from_x_formation_count};
 
     #[test]
     fn returns_expected_word_search_count_for_test_data() {
@@ -102,5 +145,17 @@ mod tests {
     fn returns_expected_word_search_count_for_input_data() {
         let count = get_word_search_count("./input.txt", "XMAS");
         assert_eq!(count, 2500);
+    }
+
+    #[test]
+    fn returns_expected_word_search_count_in_x_formation_for_test_data() {
+        let count = get_word_search_from_x_formation_count("./test.txt", "MAS");
+        assert_eq!(count, 9);
+    }
+
+    #[test]
+    fn returns_expected_word_search_count_in_x_formation_for_input_data() {
+        let count = get_word_search_from_x_formation_count("./input.txt", "MAS");
+        assert_eq!(count, 1933);
     }
 }
