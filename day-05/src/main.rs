611650c93fa2174aba25ac7451c72276178cc574
Dan Barclay <barclaysd@me.com>
2024-12-05 23:38:47 +0000

    feat: day 5 part 2

--- day-05/src/main.rs
+++ day-05/src/main.rs
@@ -1,13 +1,12 @@
+use std::cmp::PartialEq;
 use std::collections::{HashMap, HashSet};
 use std::fs;
+use crate::Part::{Part1, Part2};
 
-fn parse_page_number_rules(input: &str) -> Result<(i32, i32), String> {
-    let numbers: Result<Vec<i32>, _> = input.split('|').map(str::parse::<i32>).collect();
-
-    match numbers {
-        Ok(nums) if nums.len() == 2 => Ok((nums[0], nums[1])),
-        _ => Err("Invalid input format".to_string()),
-    }
+#[derive(PartialEq, Debug)]
+enum Part {
+    Part1,
+    Part2,
 }
 
 fn build_dependency_graph(rules: &[(i32, i32)]) -> HashMap<i32, HashSet<i32>> {
@@ -19,6 +18,15 @@ fn build_dependency_graph(rules: &[(i32, i32)]) -> HashMap<i32, HashSet<i32>> {
     }
 
     graph
+}
+
+fn parse_page_number_rules(input: &str) -> Result<(i32, i32), String> {
+    let numbers: Result<Vec<i32>, _> = input.split('|').map(str::parse::<i32>).collect();
+
+    match numbers {
+        Ok(nums) if nums.len() == 2 => Ok((nums[0], nums[1])),
+        _ => Err("Invalid input format".to_string()),
+    }
 }
 
 fn get_middle_page(sequence: &[i32]) -> i32 {
@@ -45,22 +53,48 @@ fn is_valid_update_sequence(sequence: &[i32], graph: &HashMap<i32, HashSet<i32>>
     true
 }
 
-fn get_sum_of_middle_pages_for_valid_sequences(file_path: &str) -> i32 {
+fn reorder_sequence(sequence: &[i32], graph: &HashMap<i32, HashSet<i32>>) -> Vec<i32> {
+    let mut reordered_sequence: Vec<i32> = sequence.to_vec();
+
+    for &page in sequence {
+        if let Some(prerequisites) = graph.get(&page) {
+            let mut relevant_prerequisites: Vec<_> = prerequisites
+            .iter()
+            .filter(|prerequisite| sequence.contains(prerequisite))
+                .copied()
+                .collect();
+
+                relevant_prerequisites.sort();
+
+            for &prerequisite in &relevant_prerequisites {
+                let current_pos = reordered_sequence.iter().position(|&p| p == prerequisite);
+                let page_pos = reordered_sequence.iter().position(|&p| p == page);
+                
+                if let (Some(curr_pos), Some(p_pos)) = (current_pos, page_pos) {
+                    if curr_pos > p_pos {
+                        reordered_sequence.remove(curr_pos);
+                        reordered_sequence.insert(p_pos, prerequisite);
+                    }
+                }
+            }
+        }
+    }
+    
+    reordered_sequence
+}
+
+fn get_sum_of_middle_pages_for_valid_sequences(file_path: &str, part: Part) -> i32 {
     let file_contents =
         fs::read_to_string(file_path).expect("Should have been able to read the file");
 
-    let page_number_rules = file_contents
-        .split("\n\n")
-        .next()
-        .unwrap()
+    let (rules, updates) = file_contents.split_once("\n\n").unwrap();
+
+    let parsed_rules = rules
         .lines()
         .flat_map(parse_page_number_rules)
         .collect::<Vec<(i32, i32)>>();
 
-    let update_sequence = file_contents
-        .split("\n\n")
-        .nth(1)
-        .unwrap()
+    let parsed_updates = updates
         .lines()
         .map(|line| {
             line.split(',')
@@ -69,36 +103,71 @@ fn get_sum_of_middle_pages_for_valid_sequences(file_path: &str) -> i32 {
         })
         .collect::<Vec<Vec<i32>>>();
 
-    let graph = build_dependency_graph(&page_number_rules);
+    let graph = build_dependency_graph(&parsed_rules);
 
-    update_sequence
+    let part_1: i32 = parsed_updates
         .iter()
         .filter_map(|sequence| {
             is_valid_update_sequence(sequence, &graph).then(|| get_middle_page(sequence))
         })
-        .sum()
+        .sum();
+
+    let part_2: i32 = parsed_updates
+        .iter()
+        .filter_map(|sequence| {
+            let is_valid = is_valid_update_sequence(sequence, &graph);
+            if is_valid {
+                return None;
+            }
+            let reordered_sequence = reorder_sequence(sequence, &graph);
+            Some(get_middle_page(reordered_sequence.as_slice()))
+        })
+        .sum();
+    
+    if part == Part::Part1 {
+        return part_1;
+    }
+    
+    part_2
 }
 
 fn main() {
     println!(
-        "Sum of middle pages: {}",
-        get_sum_of_middle_pages_for_valid_sequences("./input.txt")
+        "Part 1: Sum of middle pages: {}",
+        get_sum_of_middle_pages_for_valid_sequences("./input.txt", Part1)
+    );
+    println!(
+        "Part 2: Sum of middle pages: {}",
+        get_sum_of_middle_pages_for_valid_sequences("./input.txt", Part2)
     );
 }
 
 #[cfg(test)]
 mod tests {
     use crate::get_sum_of_middle_pages_for_valid_sequences;
+    use crate::Part::{Part1, Part2};
 
     #[test]
-    fn returns_expected_value_test_data() {
-        let sum_of_middle_pages = get_sum_of_middle_pages_for_valid_sequences("./test.txt");
+    fn returns_expected_value_test_data_for_part_1() {
+        let sum_of_middle_pages = get_sum_of_middle_pages_for_valid_sequences("./test.txt", Part1);
         assert_eq!(sum_of_middle_pages, 143);
     }
 
     #[test]
-    fn returns_expected_value_for_input_data() {
-        let sum_of_middle_pages = get_sum_of_middle_pages_for_valid_sequences("./input.txt");
+    fn returns_expected_value_for_input_data_for_part_1() {
+        let sum_of_middle_pages = get_sum_of_middle_pages_for_valid_sequences("./input.txt", Part1);
         assert_eq!(sum_of_middle_pages, 6051);
+    }
+
+    #[test]
+    fn returns_expected_value_test_data_for_part_2() {
+        let sum_of_middle_pages = get_sum_of_middle_pages_for_valid_sequences("./test.txt", Part2);
+        assert_eq!(sum_of_middle_pages, 123);
+    }
+
+    #[test]
+    fn returns_expected_value_for_input_data_for_part_2() {
+        let sum_of_middle_pages = get_sum_of_middle_pages_for_valid_sequences("./input.txt", Part2);
+        assert_eq!(sum_of_middle_pages, 5390);
     }
 }
