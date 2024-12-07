af44fef59c2e42496b1a1b57dee41f5eb9f24724
Dan Barclay <barclaysd@me.com>
2024-12-07 11:49:27 +0000

    chore: new day script

---
+++ scripts/new-day.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+next_day=$(ls -d day-* 2>/dev/null | grep -o '[0-9]\+' | sort -n | tail -1)
+if [ -z "$next_day" ]; then
+    next_day=0
+fi
+next_day=$((next_day + 1))
+
+formatted_day=$(printf "%02d" $next_day)
+new_folder="day-$formatted_day" 
+
+mkdir $new_folder
+cargo init $new_folder
+
+cp aoc.template.rs $new_folder/src/main.rs
