80edf86373480498c344ac2e9f47781566a7e683
Dan Barclay <barclaysd@me.com>
2024-12-09 19:22:26 +0000

    script: improve new-day.sh

---
+++ day-08/src/main.rs
@@ -0,0 +1,33 @@
+use std::fs;
+
+fn get_value(file_path: &str) -> i32 {
+    let file_contents =
+        fs::read_to_string(file_path).expect("Should have been able to read the file");
+
+    for line in file_contents.lines() {
+
+    }
+
+   8
+}
+
+fn main() {
+    println!("Value: {}", get_value("./input.txt"));
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::get_value;
+
+    #[test]
+    fn returns_expected_value_test_data() {
+        let value = get_value("./test.txt");
+        assert_eq!(value, 11);
+    }
+
+    #[test]
+    fn returns_expected_value_for_input_data() {
+        let value = get_value("./input.txt");
+        assert_eq!(value, 1110981);
+    }
+}
--- scripts/new-day.sh
+++ scripts/new-day.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
-next_day=$(ls -d day-* 2>/dev/null | grep -o '[0-9]\+' | sort -n | tail -1)
-if [ -z "$next_day" ]; then
-    next_day=0
+day=$(ls -d day-* 2>/dev/null | grep -o '[0-9]\+' | sort -n | tail -1)
+if [ -z "$day" ]; then
+    day=0
 fi
-next_day=$((next_day + 1))
+day=$((day + 1))
 
-formatted_day=$(printf "%02d" $next_day)
+formatted_day=$(printf "%02d" $day)
 new_folder="day-$formatted_day" 
 
 mkdir $new_folder
 cargo init $new_folder
-touch $new_folder/input.txt
 touch $new_folder/test.txt
 
 cp aoc.template.rs $new_folder/src/main.rs
+
+export $(cat .env | xargs)
+
+url="https://adventofcode.com/2024/day/${day}"
+title="$(curl -s --cookie "session=${SESSION_COOKIE}" "$url" | pup 'article h2 text{}' | sed 's/--- Day [0-9]*: \(.*\) ---/\1/')"
+echo "| ${day} | ★★ | [Day ${day}: ${title}](https://adventofcode.com/2024/day/${day})      |" >> README.md
