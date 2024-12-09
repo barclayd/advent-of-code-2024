905685ad4e08809874cc6b95445090b7c86bd054
Dan Barclay <barclaysd@me.com>
2024-12-09 17:59:56 +0000

    feat: day 07 part 2

--- day-07/src/main.rs
+++ day-07/src/main.rs
@@ -1,7 +1,21 @@
 use std::collections::HashMap;
 use std::fs;
+use crate::Part::{Part1, Part2};
 
-fn get_is_calibrated(numbers: &Vec<i64>, target: i64) -> bool {
+#[derive(PartialEq, Debug, Clone)]
+enum Part {
+    Part1,
+    Part2,
+}
+
+fn concatenation_operator(first_number: i64, second_number: i64) -> i64 {
+    let joined_number = format!("{}{}", first_number, second_number)
+    .parse()
+        .expect("Should have been able to parse the joined number");
+    joined_number
+}
+
+fn get_is_calibrated(numbers: &Vec<i64>, target: i64, part: &Part) -> bool {
     if numbers.len() == 1 {
         return numbers[0] == target;
     }
@@ -14,6 +28,7 @@ fn get_is_calibrated(numbers: &Vec<i64>, target: i64) -> bool {
         numbers: &[i64],
         target: i64,
         cache: &mut HashMap<(usize, i64), bool>,
+        part: &Part,
     ) -> bool {
         if pos == numbers.len() {
             return current == target;
@@ -24,17 +39,25 @@ fn get_is_calibrated(numbers: &Vec<i64>, target: i64) -> bool {
         }
 
         let num = numbers[pos];
-        let result = calibrate(pos + 1, current + num, numbers, target, cache)
-            || calibrate(pos + 1, current * num, numbers, target, cache);
+        let result;
+        
+        if part == &Part1 {
+            result =  calibrate(pos + 1, current + num, numbers, target, cache, part)
+                || calibrate(pos + 1, current * num, numbers, target, cache, part);
+        } else {
+            result = calibrate(pos + 1, current + num, numbers, target, cache, part)
+                || calibrate(pos + 1, current * num, numbers, target, cache, part)
+                || calibrate(pos + 1, concatenation_operator(current, num), numbers, target, cache, part);
+        }
 
         cache.insert((pos, current), result);
         result
     }
 
-    calibrate(1, numbers[0], numbers, target, &mut cache)
+    calibrate(1, numbers[0], numbers, target, &mut cache, part)
 }
 
-fn get_total_calibration_result(file_path: &str) -> i64 {
+fn get_total_calibration_result(file_path: &str, part: Part) -> i64 {
     let file_contents =
         fs::read_to_string(file_path).expect("Should have been able to read the file");
 
@@ -54,30 +77,47 @@ fn get_total_calibration_result(file_path: &str) -> i64 {
 
     parsed_lines
         .into_iter()
-        .filter_map(|(target, numbers)| get_is_calibrated(&numbers, target).then_some(target))
+        .filter_map(|(target, numbers)| get_is_calibrated(&numbers, target, &part).then_some(target))
         .sum()
 }
 
 fn main() {
     println!(
-        "Total calibration result: {}",
-        get_total_calibration_result("./input.txt")
+        "Total calibration result for part 1: {}",
+        get_total_calibration_result("./input.txt", Part1)
+    );
+    println!(
+        "Total calibration result for part 2: {}",
+        get_total_calibration_result("./input.txt", Part2)
     );
 }
 
 #[cfg(test)]
 mod tests {
     use crate::get_total_calibration_result;
+    use crate::Part::{Part1, Part2};
 
     #[test]
-    fn returns_expected_value_test_data() {
-        let total_calibration_result = get_total_calibration_result("./test.txt");
+    fn returns_expected_value_test_data_for_part_1() {
+        let total_calibration_result = get_total_calibration_result("./test.txt", Part1);
         assert_eq!(total_calibration_result, 3749);
     }
 
     #[test]
-    fn returns_expected_value_for_input_data() {
-        let value = get_total_calibration_result("./input.txt");
-        assert_eq!(value, 303876485655);
+    fn returns_expected_value_for_input_data_for_part_1() {
+        let total_calibration_result = get_total_calibration_result("./input.txt", Part1);
+        assert_eq!(total_calibration_result, 303876485655);
+    }
+
+    #[test]
+    fn returns_expected_value_test_data_for_part_2() {
+        let total_calibration_result = get_total_calibration_result("./test.txt", Part2);
+        assert_eq!(total_calibration_result, 11387);
+    }
+
+    #[test]
+    fn returns_expected_value_for_input_data_for_part_2() {
+        let total_calibration_result = get_total_calibration_result("./input.txt", Part2);
+        assert_eq!(total_calibration_result, 146111650210682);
     }
 }
