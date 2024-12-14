bf2a1183083fd2da0c011216aefb28b1b7ed8835
Dan Barclay <barclaysd@me.com>
2024-12-14 01:49:04 +0000

    feat: day 13 part 2 solution

--- README.md
+++ README.md
@@ -62,20 +62,20 @@ advent-of-code-2024/
 └── README.md
 ```
 
-## Progress (24/50 ⭐️)
+## Progress (26/50 ⭐️)
 
 | Day | Challenge                                                           | Stars |
-|-----|---------------------------------------------------------------------|-------|
-| 1   | [Historian Hysteria](https://adventofcode.com/2024/day/1)         | ⭐️⭐️  |
-| 2   | [Red-Nosed Reports](https://adventofcode.com/2024/day/2)          | ⭐️⭐️  |
-| 3   | [Mull It Over](https://adventofcode.com/2024/day/3)               | ⭐️⭐️  |
-| 4   | [Ceres Search](https://adventofcode.com/2024/day/4)               | ⭐️⭐️  |
-| 5   | [Print Queue](https://adventofcode.com/2024/day/5)                | ⭐️⭐️  |
-| 6   | [Guard Gallivant](https://adventofcode.com/2024/day/6)            | ⭐️⭐️  |
-| 7   | [Bridge Repair](https://adventofcode.com/2024/day/7)              | ⭐️⭐️  |
-| 8   | [Resonant Collinearity](https://adventofcode.com/2024/day/8)      | ⭐⭐    |
-| 9   | [Disk Fragmenter](https://adventofcode.com/2024/day/9) | ⭐️⭐️  |
-| 10   | [Hoof It](https://adventofcode.com/2024/day/10) | ⭐️⭐   |
-| 11   | [Plutonian Pebbles](https://adventofcode.com/2024/day/11) | ⭐️⭐   |
-| 12   | [Garden Groups](https://adventofcode.com/2024/day/12) | ⭐️⭐   |
-| 13   | [Claw Contraption](https://adventofcode.com/2024/day/13) | ⭐️★   |
+|-----|---------------------------------------------------------------------|------|
+| 1   | [Historian Hysteria](https://adventofcode.com/2024/day/1)         | ⭐️⭐️ |
+| 2   | [Red-Nosed Reports](https://adventofcode.com/2024/day/2)          | ⭐️⭐️ |
+| 3   | [Mull It Over](https://adventofcode.com/2024/day/3)               | ⭐️⭐️ |
+| 4   | [Ceres Search](https://adventofcode.com/2024/day/4)               | ⭐️⭐️ |
+| 5   | [Print Queue](https://adventofcode.com/2024/day/5)                | ⭐️⭐️ |
+| 6   | [Guard Gallivant](https://adventofcode.com/2024/day/6)            | ⭐️⭐️ |
+| 7   | [Bridge Repair](https://adventofcode.com/2024/day/7)              | ⭐️⭐️ |
+| 8   | [Resonant Collinearity](https://adventofcode.com/2024/day/8)      | ⭐⭐   |
+| 9   | [Disk Fragmenter](https://adventofcode.com/2024/day/9) | ⭐️⭐️ |
+| 10   | [Hoof It](https://adventofcode.com/2024/day/10) | ⭐️⭐  |
+| 11   | [Plutonian Pebbles](https://adventofcode.com/2024/day/11) | ⭐️⭐  |
+| 12   | [Garden Groups](https://adventofcode.com/2024/day/12) | ⭐️⭐  |
+| 13   | [Claw Contraption](https://adventofcode.com/2024/day/13) | ⭐⭐   |
--- day-13/src/main.rs
+++ day-13/src/main.rs
@@ -1,6 +1,6 @@
+use crate::Part::{Part1, Part2};
 use std::fs;
 use std::str::FromStr;
-use crate::Part::{Part1, Part2};
 
 #[derive(PartialEq, Debug)]
 enum Part {
@@ -31,9 +31,9 @@ impl FromStr for Position {
         }
 
         let [px, py] = parts[..2] else {
-            return Err("Invalid coordinate format".to_string())
+            return Err("Invalid coordinate format".to_string());
         };
-        
+
         Ok(Position {
             x: parse_coordinate(px, "X")?,
             y: parse_coordinate(py, "Y")?,
@@ -64,11 +64,16 @@ impl FromStr for Claw {
 
 impl Claw {
     fn calculate_tokens(&self) -> Option<i64> {
-       let n = (self.button_a.x * self.prize.y - self.button_a.y * self.prize.x) / (self.button_a.x * self.button_b.y - self.button_a.y * self.button_b.x);
-       let m = (self.prize.x - self.button_b.x * n) / self.button_a.x;
+        let n = (self.button_a.x * self.prize.y - self.button_a.y * self.prize.x)
+            / (self.button_a.x * self.button_b.y - self.button_a.y * self.button_b.x);
+        let m = (self.prize.x - self.button_b.x * n) / self.button_a.x;
 
-        if (self.button_a.x * m + self.button_b.x * n, self.button_a.y * m + self.button_b.y * n) == (self.prize.x, self.prize.y) {
-            return Some(3*m + n)
+        if (
+            self.button_a.x * m + self.button_b.x * n,
+            self.button_a.y * m + self.button_b.y * n,
+        ) == (self.prize.x, self.prize.y)
+        {
+            Some(3 * m + n)
         } else {
             None
         }
@@ -87,6 +92,7 @@ fn parse_value(line: &str) -> Result<Position, String> {
     line.split(": ")
         .nth(1)
         .ok_or_else(|| "Invalid prize format".to_string())?
+        
         .parse()
 }
 
@@ -99,20 +105,38 @@ fn parse_input(input: &str) -> Result<Vec<Claw>, String> {
 }
 
 fn get_minimum_amount_of_tokens_spent_to_win_all_prizes(file_path: &str, part: Part) -> i64 {
-    let file_contents = fs::read_to_string(file_path)
-        .expect("Should have been able to read the file");
+    let file_contents =
+        fs::read_to_string(file_path).expect("Should have been able to read the file");
 
-    let claws = parse_input(&file_contents)
-        .expect("Failed to parse input");
+    let claws = parse_input(&file_contents).expect("Failed to parse input");
 
     if part == Part1 {
         claws.iter().filter_map(Claw::calculate_tokens).sum()
-    } else { 4 }
+    } else {
+        claws.iter()
+        .filter_map(|claw| {
+            let new_claw = Claw {
+                prize: Position {
+                    x: claw.prize.x + 10_000_000_000_000,
+                    y: claw.prize.y + 10_000_000_000_000
+                },
+                ..*claw
+            };
+            Claw::calculate_tokens(&new_claw)
+        })
+        .sum()
+    }
 }
 
 fn main() {
-    println!("Part 1 value: {}", get_minimum_amount_of_tokens_spent_to_win_all_prizes("./input.txt", Part1));
-    println!("Part 2 value: {}", get_minimum_amount_of_tokens_spent_to_win_all_prizes("./test.txt", Part2));
+    println!(
+        "Part 1 value: {}",
+        get_minimum_amount_of_tokens_spent_to_win_all_prizes("./input.txt", Part1)
+    );
+    println!(
+        "Part 2 value: {}",
+        get_minimum_amount_of_tokens_spent_to_win_all_prizes("./test.txt", Part2)
+    );
 }
 
 #[cfg(test)]
@@ -122,25 +146,29 @@ mod tests {
 
     #[test]
     fn returns_expected_value_test_data_for_part_1() {
-        let minimum_amount_of_tokens_spent_to_win_prizes = get_minimum_amount_of_tokens_spent_to_win_all_prizes("./test.txt", Part1);
+        let minimum_amount_of_tokens_spent_to_win_prizes =
+            get_minimum_amount_of_tokens_spent_to_win_all_prizes("./test.txt", Part1);
         assert_eq!(minimum_amount_of_tokens_spent_to_win_prizes, 480);
     }
 
     #[test]
     fn returns_expected_value_for_input_data_for_part_1() {
-        let minimum_amount_of_tokens_spent_to_win_prizes = get_minimum_amount_of_tokens_spent_to_win_all_prizes("./input.txt", Part1);
+        let minimum_amount_of_tokens_spent_to_win_prizes =
+            get_minimum_amount_of_tokens_spent_to_win_all_prizes("./input.txt", Part1);
         assert_eq!(minimum_amount_of_tokens_spent_to_win_prizes, 40369);
     }
 
     #[test]
     fn returns_expected_value_test_data_for_part_2() {
-        let minimum_amount_of_tokens_spent_to_win_prizes = get_minimum_amount_of_tokens_spent_to_win_all_prizes("./test.txt", Part2);
-        assert_eq!(minimum_amount_of_tokens_spent_to_win_prizes, 4);
+        let minimum_amount_of_tokens_spent_to_win_prizes =
+            get_minimum_amount_of_tokens_spent_to_win_all_prizes("./test.txt", Part2);
+        assert_eq!(minimum_amount_of_tokens_spent_to_win_prizes, 875318608908);
     }
 
     #[test]
     fn returns_expected_value_for_input_data_for_part_2() {
-        let minimum_amount_of_tokens_spent_to_win_prizes = get_minimum_amount_of_tokens_spent_to_win_all_prizes("./input.txt", Part2);
-        assert_eq!(minimum_amount_of_tokens_spent_to_win_prizes, 4);
+        let minimum_amount_of_tokens_spent_to_win_prizes =
+            get_minimum_amount_of_tokens_spent_to_win_all_prizes("./input.txt", Part2);
+        assert_eq!(minimum_amount_of_tokens_spent_to_win_prizes, 72587986598368);
     }
 }
