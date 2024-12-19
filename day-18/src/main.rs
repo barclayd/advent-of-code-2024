65f321baf644b9f556c60725f2a8d896b0164b7d
Dan Barclay <barclaysd@me.com>
2024-12-19 01:16:27 +0000

    feat: day 18 part 2

--- day-18/src/main.rs
+++ day-18/src/main.rs
@@ -4,6 +4,23 @@ use std::fs;
 
 const DIRS: [(i64, i64); 4] = [(0, -1), (1, 0), (0, 1), (-1, 0)];
 
+fn search(list: Vec<(i64, i64)>, limit: usize) -> Option<(Vec<(i64, i64)>, i64)> {
+    let memory: HashSet<&(i64, i64)> = list.iter().take(limit).collect();
+
+    astar(
+        &(0i64, 0i64),
+        |state| {
+            DIRS.iter()
+                .map(|dir| ((state.0 + dir.0, state.1 + dir.1), 1))
+                .filter(|(pos, _)| !memory.contains(&pos))
+                .filter(|(pos, _)| pos.0 >= 0 && pos.1 >= 0 && pos.0 <= 70 && pos.1 <= 70)
+                .collect::<Vec<_>>()
+        },
+        |state| (70 - state.0) + (70 - state.1),
+        |state| *state == (70, 70),
+    )
+}
+
 fn get_minimum_steps(file_path: &str) -> i64 {
     let file_contents =
         fs::read_to_string(file_path).expect("Should have been able to read the file");
@@ -16,33 +33,41 @@ fn get_minimum_steps(file_path: &str) -> i64 {
         })
         .collect();
 
-    let memory: HashSet<&(i64, i64)> = list.iter().take(1024).collect();
-    let Some((_path, cost)) = astar(
-        &(0i64, 0i64),
-        |state| {
-            DIRS.iter()
-                .map(|dir| ((state.0 + dir.0, state.1 + dir.1), 1))
-                .filter(|(pos, _)| !memory.contains(&pos))
-                .filter(|(pos, _)| pos.0 >= 0 && pos.1 >= 0 && pos.0 <= 70 && pos.1 <= 70)
-                .collect::<Vec<_>>()
-        },
-        |state| (70 - state.0) + (70 - state.1),
-        |state| *state == (70, 70),
-    ) else {
-        panic!("No path found")
-    };
+    match search(list, 1024) {
+        Some((_, cost)) => cost,
+        None => panic!("No solution found"),
+    }
+}
 
-    cost
+fn get_coordinates(file_path: &str) -> String {
+    let file_contents =
+        fs::read_to_string(file_path).expect("Should have been able to read the file");
+
+    let list: Vec<(i64, i64)> = file_contents
+        .lines()
+        .filter_map(|line| {
+            line.split_once(',')
+                .and_then(|(x, y)| Some((x.trim().parse().ok()?, y.trim().parse().ok()?)))
+        })
+        .collect();
+
+    for limit in 1024..list.len() {
+        if search(list.clone(), limit).is_none() {
+            return format!("{},{}", list[limit-1].0, list[limit-1].1)
+        }
+    }
+
+    panic!("No solution found");
 }
 
 fn main() {
     println!("Part 1 value: {}", get_minimum_steps("./input.txt"));
-    println!("Part 2 value: {}", get_minimum_steps("./input.txt"));
+    println!("Part 2 value: {}", get_coordinates("./input.txt"));
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::get_minimum_steps;
+    use crate::{get_coordinates, get_minimum_steps};
 
     #[test]
     fn returns_expected_value_test_data_for_part_1() {
@@ -57,14 +82,8 @@ mod tests {
     }
 
     #[test]
-    fn returns_expected_value_test_data_for_part_2() {
-        let value = get_minimum_steps("./test.txt");
-        assert_eq!(value, 4);
-    }
-
-    #[test]
     fn returns_expected_value_for_input_data_for_part_2() {
-        let value = get_minimum_steps("./input.txt");
-        assert_eq!(value, 4);
+        let value = get_coordinates("./input.txt");
+        assert_eq!(value, "30,12");
     }
 }
