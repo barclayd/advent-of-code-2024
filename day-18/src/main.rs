dfda238f8369f54cbe7090376447e9fbbf675b7b
Dan Barclay <barclaysd@me.com>
2024-12-19 00:58:12 +0000

    chore: refactor

--- day-18/src/main.rs
+++ day-18/src/main.rs
@@ -1,17 +1,10 @@
-use crate::Part::{Part1, Part2};
 use pathfinding::prelude::astar;
 use std::collections::HashSet;
 use std::fs;
 
-#[derive(PartialEq, Debug)]
-enum Part {
-    Part1,
-    Part2,
-}
-
 const DIRS: [(i64, i64); 4] = [(0, -1), (1, 0), (0, 1), (-1, 0)];
 
-fn get_value(file_path: &str, part: Part) -> i64 {
+fn get_minimum_steps(file_path: &str) -> i64 {
     let file_contents =
         fs::read_to_string(file_path).expect("Should have been able to read the file");
 
@@ -29,8 +22,8 @@ fn get_value(file_path: &str, part: Part) -> i64 {
         |state| {
             DIRS.iter()
                 .map(|dir| ((state.0 + dir.0, state.1 + dir.1), 1))
-                .filter(|pos| !memory.contains(&pos.0))
-                .filter(|pos| pos.0.0 >= 0 && pos.0.1 >= 0 && pos.0.0 <= 70 && pos.0.1 <= 70)
+                .filter(|(pos, _)| !memory.contains(&pos))
+                .filter(|(pos, _)| pos.0 >= 0 && pos.1 >= 0 && pos.0 <= 70 && pos.1 <= 70)
                 .collect::<Vec<_>>()
         },
         |state| (70 - state.0) + (70 - state.1),
@@ -38,45 +31,40 @@ fn get_value(file_path: &str, part: Part) -> i64 {
     ) else {
         panic!("No path found")
     };
-    
-    if part == Part1 {
-        cost
-    } else {
-        4
-    }
+
+    cost
 }
 
 fn main() {
-    println!("Part 1 value: {}", get_value("./input.txt", Part1));
-    println!("Part 2 value: {}", get_value("./input.txt", Part2));
+    println!("Part 1 value: {}", get_minimum_steps("./input.txt"));
+    println!("Part 2 value: {}", get_minimum_steps("./input.txt"));
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::get_value;
-    use crate::Part::{Part1, Part2};
+    use crate::get_minimum_steps;
 
     #[test]
     fn returns_expected_value_test_data_for_part_1() {
-        let value = get_value("./test.txt", Part1);
+        let value = get_minimum_steps("./test.txt");
         assert_eq!(value, 146);
     }
 
     #[test]
     fn returns_expected_value_for_input_data_for_part_1() {
-        let value = get_value("./input.txt", Part1);
+        let value = get_minimum_steps("./input.txt");
         assert_eq!(value, 374);
     }
 
     #[test]
     fn returns_expected_value_test_data_for_part_2() {
-        let value = get_value("./test.txt", Part2);
+        let value = get_minimum_steps("./test.txt");
         assert_eq!(value, 4);
     }
 
     #[test]
     fn returns_expected_value_for_input_data_for_part_2() {
-        let value = get_value("./input.txt", Part2);
+        let value = get_minimum_steps("./input.txt");
         assert_eq!(value, 4);
     }
 }
