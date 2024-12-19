4968fec62c40cf513ff50b997aaf330f7cc60ed0
Dan Barclay <barclaysd@me.com>
2024-12-19 00:52:24 +0000

    feat: day 18 part 1

--- day-18/src/main.rs
+++ day-18/src/main.rs
@@ -1,5 +1,7 @@
-use std::fs;
 use crate::Part::{Part1, Part2};
+use pathfinding::prelude::astar;
+use std::collections::HashSet;
+use std::fs;
 
 #[derive(PartialEq, Debug)]
 enum Part {
@@ -7,15 +9,41 @@ enum Part {
     Part2,
 }
 
-fn get_value(file_path: &str, part: Part) -> i32 {
+const DIRS: [(i64, i64); 4] = [(0, -1), (1, 0), (0, 1), (-1, 0)];
+
+fn get_value(file_path: &str, part: Part) -> i64 {
     let file_contents =
         fs::read_to_string(file_path).expect("Should have been able to read the file");
 
-    for line in file_contents.lines() {
+    let list: Vec<(i64, i64)> = file_contents
+        .lines()
+        .filter_map(|line| {
+            line.split_once(',')
+                .and_then(|(x, y)| Some((x.trim().parse().ok()?, y.trim().parse().ok()?)))
+        })
+        .collect();
 
+    let memory: HashSet<&(i64, i64)> = list.iter().take(1024).collect();
+    let Some((_path, cost)) = astar(
+        &(0i64, 0i64),
+        |state| {
+            DIRS.iter()
+                .map(|dir| ((state.0 + dir.0, state.1 + dir.1), 1))
+                .filter(|pos| !memory.contains(&pos.0))
+                .filter(|pos| pos.0.0 >= 0 && pos.0.1 >= 0 && pos.0.0 <= 70 && pos.0.1 <= 70)
+                .collect::<Vec<_>>()
+        },
+        |state| (70 - state.0) + (70 - state.1),
+        |state| *state == (70, 70),
+    ) else {
+        panic!("No path found")
+    };
+    
+    if part == Part1 {
+        cost
+    } else {
+        4
     }
-
-   if part == Part1 { 8 } else { 4 }
 }
 
 fn main() {
@@ -31,13 +59,13 @@ mod tests {
     #[test]
     fn returns_expected_value_test_data_for_part_1() {
         let value = get_value("./test.txt", Part1);
-        assert_eq!(value, 8);
+        assert_eq!(value, 146);
     }
 
     #[test]
     fn returns_expected_value_for_input_data_for_part_1() {
         let value = get_value("./input.txt", Part1);
-        assert_eq!(value, 8);
+        assert_eq!(value, 374);
     }
 
     #[test]
